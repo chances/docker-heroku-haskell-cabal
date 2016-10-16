61d47cd767d34a15cd9cc82db4fb23d7f706f0c9
Chance Snow <git@chancesnow.me>
2016-10-16 12:58:25 -0700

    Begin conversion to reusable docker image

--- src/App.hs
+++ src/App.hs
@@ -3,47 +3,14 @@
 
 module App (run) where
 
-import Models.User
-
-import Data.List
-import Data.Time.Calendar (fromGregorian)
-
-import Servant
-import Web.Internal.HttpApiData (parseBoundedTextData)
+import Network.Wai (responseLBS, Application)
 import qualified Network.Wai.Handler.Warp as Warp
-
-type UserAPI = "users" :> QueryParam "sortby" SortBy :> Get '[JSON] [User]
-
-data SortBy = Age | Name | Email | Registration deriving (Show, Bounded, Enum)
-
-instance FromHttpApiData SortBy where
-    parseUrlPiece = parseBoundedTextData
-    parseQueryParam = parseBoundedTextData
-
-sortedBy :: Ord b => [a] -> (a -> b) -> [a]
-sortedBy list projector = sortOn projector list
-
-users :: [User]
-users =
-    [ User "Isaac Newton"    372 "isaac@newton.co.uk" (fromGregorian 1683  3 1)
-    , User "Albert Einstein" 136 "ae@mc2.org"         (fromGregorian 1905 12 1)
-    ]
-
-server :: Server UserAPI
-server = users'
-    where users' :: Maybe SortBy -> Handler [User]
-          users' sortKey = case sortKey of
-            Nothing -> return users
-            Just Age -> return $ users `sortedBy` age
-            Just Name -> return $ users `sortedBy` name
-            Just Email -> return $ users `sortedBy` email
-            Just Registration -> return $ users `sortedBy` registrationDate
-
-userAPI :: Proxy UserAPI
-userAPI = Proxy
+import Network.HTTP.Types (status200)
+import Network.HTTP.Types.Header (hContentType)
 
 app :: Application
-app = serve userAPI server
+app req f =
+    f $ responseLBS status200 [(hContentType, "text/plain")] "Hello world!"
 
 run :: Warp.Port -> IO ()
 run port = Warp.run port app
